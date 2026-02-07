257ef7a63f990ab4fb1857062362f260d92849ad
KestelG <drybitva@mail.ru>
2026-02-07 18:46:28 +0300

    Added a DDS support and bitmap out

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,11 @@ use image::{
     codecs::ico::IcoEncoder,
     codecs::bmp::BmpEncoder,
     codecs::tiff::TiffEncoder,
-    ColorType, Rgb, ImageEncoder
+    ColorType, Rgb, ImageEncoder,
 };
-use std::io::Cursor;
+use std::io::{Cursor, Write};
 use thiserror::Error;
+use ddsfile::{Dds, NewDxgiParams, DxgiFormat, Caps2, AlphaMode, D3D10ResourceDimension};
 
 #[derive(Error, Debug)]
 pub enum ConversionError {
@@ -26,6 +27,15 @@ pub enum ConversionError {
     
     #[error("Unsupported operation: {0}")]
     UnsupportedOperation(String),
+    
+    #[error("IO error: {0}")]
+    IoError(#[from] std::io::Error),
+    
+    #[error("DDS error: {0}")]
+    DdsError(String),
+    
+    #[error("Farbfeld error: {0}")]
+    FarbfeldError(String),
 }
 
 pub type Result<T> = std::result::Result<T, ConversionError>;
@@ -79,6 +89,7 @@ pub struct CompressionSettings {
     pub png_compression: u8,
     pub png_filter: PngFilterType,
     pub ico_settings: IcoSettings,
+    pub dds_mipmaps: bool,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -108,6 +119,7 @@ impl Default for CompressionSettings {
                 max_dimension: 256,
                 generate_multiple_sizes: false,
             },
+            dds_mipmaps: true,
         }
     }
 }
@@ -173,6 +185,11 @@ impl ImageConverter {
     
     pub fn with_ico_settings(mut self, settings: IcoSettings) -> Self {
         self.compression.ico_settings = settings;
+        self
+    }
+    
+    pub fn with_dds_mipmaps(mut self, mipmaps: bool) -> Self {
+        self.compression.dds_mipmaps = mipmaps;
         self
     }
     
@@ -285,7 +302,32 @@ impl ImageConverter {
     }
     
     fn encode_dds(&self, img: &DynamicImage, output: &mut Cursor<Vec<u8>>) -> Result<()> {
-        img.write_to(output, ImageFormat::Dds)?;
+        let rgba_img = img.to_rgba8();
+        let width = rgba_img.width();
+        let height = rgba_img.height();
+        let raw_data = rgba_img.into_raw();
+
+        let params = NewDxgiParams {
+            width,
+            height,
+            format: DxgiFormat::R8G8B8A8_UNorm,
+            mipmap_levels: if self.compression.dds_mipmaps { Some(0) } else { None },
+            array_layers: Some(1),
+            caps2: Some(Caps2::empty()),
+            alpha_mode: AlphaMode::Straight,
+            resource_dimension: D3D10ResourceDimension::Texture2D,
+            depth: Some(1),
+            is_cubemap: false,
+        };
+
+        let mut dds = Dds::new_dxgi(params)
+            .map_err(|e| ConversionError::DdsError(e.to_string()))?;
+
+        dds.data = raw_data;
+
+        dds.write(output)
+            .map_err(|e| ConversionError::DdsError(e.to_string()))?;
+
         Ok(())
     }
     
@@ -308,7 +350,33 @@ impl ImageConverter {
     }
     
     fn encode_farbfeld(&self, img: &DynamicImage, output: &mut Cursor<Vec<u8>>) -> Result<()> {
-        img.write_to(output, ImageFormat::Farbfeld)?;
+        let rgba_img = img.to_rgba16();
+        let width = rgba_img.width();
+        let height = rgba_img.height();
+        let raw_data = rgba_img.into_raw();
+        
+        if width == 0 || height == 0 || width > 0xFFFF || height > 0xFFFF {
+            return Err(ConversionError::FarbfeldError(
+                "Invalid image dimensions".to_string()
+            ));
+        }
+        
+        output.write_all(b"farbfeld")?;
+        output.write_all(&(width as u32).to_be_bytes())?;
+        output.write_all(&(height as u32).to_be_bytes())?;
+        
+        for chunk in raw_data.chunks_exact(4) {
+            let r = u16::from(chunk[0]);
+            let g = u16::from(chunk[1]);
+            let b = u16::from(chunk[2]);
+            let a = u16::from(chunk[3]);
+            
+            output.write_all(&r.to_be_bytes())?;
+            output.write_all(&g.to_be_bytes())?;
+            output.write_all(&b.to_be_bytes())?;
+            output.write_all(&a.to_be_bytes())?;
+        }
+        
         Ok(())
     }
     
@@ -371,6 +439,122 @@ pub enum ImageColorType {
     Unknown,
 }
 
+#[repr(C)]
+pub struct RawPixels {
+    pub data: *mut u8,
+    pub len: usize,
+    pub capacity: usize,
+    pub width: u32,
+    pub height: u32,
+    pub channels: u32,
+    pub color_type: ImageColorType,
+}
+
+impl RawPixels {
+    pub fn new(data: Vec<u8>, width: u32, height: u32, channels: u32, color_type: ImageColorType) -> Self {
+        let len = data.len();
+        let capacity = data.capacity();
+        let data_ptr = data.as_ptr() as *mut u8;
+        std::mem::forget(data);
+        
+        Self {
+            data: data_ptr,
+            len,
+            capacity,
+            width,
+            height,
+            channels,
+            color_type,
+        }
+    }
+}
+
+pub fn free_raw_pixels(pixels: RawPixels) {
+    if !pixels.data.is_null() {
+        unsafe {
+            Vec::from_raw_parts(pixels.data, pixels.len, pixels.capacity);
+        }
+    }
+}
+
+pub fn to_bitmap(input_data: &[u8]) -> Result<Vec<u8>> {
+    let converter = ImageConverter::new(Format::Bmp);
+    converter.convert(input_data)
+}
+
+pub fn get_raw_pixels(input_data: &[u8]) -> Result<RawPixels> {
+    let img = image::load_from_memory(input_data)?;
+    
+    match img.color() {
+        ColorType::L8 | ColorType::L16 => {
+            let gray_img = img.to_luma8();
+            let width = gray_img.width();
+            let height = gray_img.height();
+            let data = gray_img.into_raw();
+            
+            let color_type = match img.color() {
+                ColorType::L8 => ImageColorType::Grayscale,
+                ColorType::L16 => ImageColorType::Grayscale16,
+                _ => ImageColorType::Grayscale,
+            };
+            
+            Ok(RawPixels::new(data, width, height, 1, color_type))
+        }
+        ColorType::La8 | ColorType::La16 => {
+            let gray_alpha_img = img.to_luma_alpha8();
+            let width = gray_alpha_img.width();
+            let height = gray_alpha_img.height();
+            let data = gray_alpha_img.into_raw();
+            
+            let color_type = match img.color() {
+                ColorType::La8 => ImageColorType::GrayscaleAlpha,
+                ColorType::La16 => ImageColorType::GrayscaleAlpha16,
+                _ => ImageColorType::GrayscaleAlpha,
+            };
+            
+            Ok(RawPixels::new(data, width, height, 2, color_type))
+        }
+        ColorType::Rgb8 | ColorType::Rgb16 | ColorType::Rgb32F => {
+            let rgb_img = img.to_rgb8();
+            let width = rgb_img.width();
+            let height = rgb_img.height();
+            let data = rgb_img.into_raw();
+            
+            let color_type = match img.color() {
+                ColorType::Rgb8 => ImageColorType::Rgb,
+                ColorType::Rgb16 => ImageColorType::Rgb16,
+                ColorType::Rgb32F => ImageColorType::Rgb32F,
+                _ => ImageColorType::Rgb,
+            };
+            
+            Ok(RawPixels::new(data, width, height, 3, color_type))
+        }
+        ColorType::Rgba8 | ColorType::Rgba16 | ColorType::Rgba32F => {
+            let rgba_img = img.to_rgba8();
+            let width = rgba_img.width();
+            let height = rgba_img.height();
+            let data = rgba_img.into_raw();
+            
+            let color_type = match img.color() {
+                ColorType::Rgba8 => ImageColorType::Rgba,
+                ColorType::Rgba16 => ImageColorType::Rgba16,
+                ColorType::Rgba32F => ImageColorType::Rgba32F,
+                _ => ImageColorType::Rgba,
+            };
+            
+            Ok(RawPixels::new(data, width, height, 4, color_type))
+        }
+        _ => {
+            let rgba_img = img.to_rgba8();
+            let width = rgba_img.width();
+            let height = rgba_img.height();
+            let data = rgba_img.into_raw();
+            
+            Ok(RawPixels::new(data, width, height, 4, ImageColorType::Rgba))
+        }
+    }
+}
+
 pub mod ffi {
     use super::*;
     use std::slice;
@@ -392,6 +576,8 @@ pub mod ffi {
         ErrorInvalidParameter = 4,
         ErrorNullPointer = 5,
         ErrorUnsupportedOperation = 6,
+        ErrorDds = 7,
+        ErrorFarbfeld = 8,
     }
     
     #[repr(C)]
@@ -425,6 +611,7 @@ pub mod ffi {
         png_filter: PngFilterType,
         ico_max_dimension: u32,
         ico_generate_multiple: bool,
+        dds_mipmaps: bool,
         output: *mut *mut ImageData,
     ) -> ResultCode {
         if input_data.is_null() || output.is_null() {
@@ -441,6 +628,7 @@ pub mod ffi {
                 max_dimension: if ico_max_dimension == 0 { 256 } else { ico_max_dimension },
                 generate_multiple_sizes: ico_generate_multiple,
             },
+            dds_mipmaps,
         };
         
         let converter = ImageConverter::new(output_format)
@@ -468,6 +656,9 @@ pub mod ffi {
                 ConversionError::EncodingError(_) => ResultCode::ErrorEncoding,
                 ConversionError::ImageError(_) => ResultCode::ErrorImageProcessing,
                 ConversionError::UnsupportedOperation(_) => ResultCode::ErrorUnsupportedOperation,
+                ConversionError::DdsError(_) => ResultCode::ErrorDds,
+                ConversionError::FarbfeldError(_) => ResultCode::ErrorFarbfeld,
+                ConversionError::IoError(_) => ResultCode::ErrorEncoding,
             }
         }
     }
@@ -491,6 +682,7 @@ pub mod ffi {
             png_filter,
             256,
             false,
+            true,
             output,
         )
     }
@@ -535,6 +727,81 @@ pub mod ffi {
         }
         (*img).len
     }
+    
+    #[no_mangle]
+    pub unsafe extern "C" fn image_get_raw_pixels(
+        input_data: *const u8,
+        input_len: usize,
+        raw_pixels: *mut RawPixels,
+    ) -> ResultCode {
+        if input_data.is_null() || raw_pixels.is_null() {
+            return ResultCode::ErrorNullPointer;
+        }
+        
+        let input_slice = slice::from_raw_parts(input_data, input_len);
+        
+        match get_raw_pixels(input_slice) {
+            Ok(pixels) => {
+                *raw_pixels = pixels;
+                ResultCode::Success
+            }
+            Err(e) => match e {
+                ConversionError::InvalidFormat => ResultCode::ErrorInvalidFormat,
+                ConversionError::InvalidParameter(_) => ResultCode::ErrorInvalidParameter,
+                ConversionError::EncodingError(_) => ResultCode::ErrorEncoding,
+                ConversionError::ImageError(_) => ResultCode::ErrorImageProcessing,
+                ConversionError::UnsupportedOperation(_) => ResultCode::ErrorUnsupportedOperation,
+                ConversionError::DdsError(_) => ResultCode::ErrorDds,
+                ConversionError::FarbfeldError(_) => ResultCode::ErrorFarbfeld,
+                ConversionError::IoError(_) => ResultCode::ErrorEncoding,
+            }
+        }
+    }
+    
+    #[no_mangle]
+    pub extern "C" fn raw_pixels_free(pixels: RawPixels) {
+        free_raw_pixels(pixels);
+    }
+    
+    #[no_mangle]
+    pub unsafe extern "C" fn raw_pixels_get_data(pixels: *const RawPixels) -> *const u8 {
+        if pixels.is_null() {
+            return ptr::null();
+        }
+        (*pixels).data
+    }
+    
+    #[no_mangle]
+    pub unsafe extern "C" fn raw_pixels_get_width(pixels: *const RawPixels) -> u32 {
+        if pixels.is_null() {
+            return 0;
+        }
+        (*pixels).width
+    }
+    
+    #[no_mangle]
+    pub unsafe extern "C" fn raw_pixels_get_height(pixels: *const RawPixels) -> u32 {
+        if pixels.is_null() {
+            return 0;
+        }
+        (*pixels).height
+    }
+    
+    #[no_mangle]
+    pub unsafe extern "C" fn raw_pixels_get_channels(pixels: *const RawPixels) -> u32 {
+        if pixels.is_null() {
+            return 0;
+        }
+        (*pixels).channels
+    }
+    
+    #[no_mangle]
+    pub unsafe extern "C" fn raw_pixels_get_len(pixels: *const RawPixels) -> usize {
+        if pixels.is_null() {
+            return 0;
+        }
+        (*pixels).len
+    }
 }
 
 #[cfg(test)]
@@ -558,5 +825,72 @@ mod tests {
         let converter = ImageConverter::new(Format::Jpeg);
         let result = converter.convert(&png_data);
         assert!(result.is_ok());
+    }
+    
+    #[test]
+    fn test_to_bitmap() {
+        let png_data = vec![
+            0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A,
+            0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52,
+            0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01,
+            0x08, 0x02, 0x00, 0x00, 0x00, 0x90, 0x77, 0x53,
+            0xDE, 0x00, 0x00, 0x00, 0x0C, 0x49, 0x44, 0x41,
+            0x54, 0x08, 0xD7, 0x63, 0xF8, 0xCF, 0xC0, 0x00,
+            0x00, 0x03, 0x01, 0x01, 0x00, 0x18, 0xDD, 0x8D,
+            0xB4, 0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4E,
+            0x44, 0xAE, 0x42, 0x60, 0x82,
+        ];
+        
+        let result = to_bitmap(&png_data);
+        assert!(result.is_ok());
+        
+        let bitmap_data = result.unwrap();
+
+        assert!(bitmap_data.len() > 2);
+        assert_eq!(bitmap_data[0], b'B');
+        assert_eq!(bitmap_data[1], b'M');
+    }
+    
+    #[test]
+    fn test_get_raw_pixels() {
+        let png_data = vec![
+            0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A,
+            0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52,
+            0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01,
+            0x08, 0x02, 0x00, 0x00, 0x00, 0x90, 0x77, 0x53,
+            0xDE, 0x00, 0x00, 0x00, 0x0C, 0x49, 0x44, 0x41,
+            0x54, 0x08, 0xD7, 0x63, 0xF8, 0xCF, 0xC0, 0x00,
+            0x00, 0x03, 0x01, 0x01, 0x00, 0x18, 0xDD, 0x8D,
+            0xB4, 0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4E,
+            0x44, 0xAE, 0x42, 0x60, 0x82,
+        ];
+        
+        let result = get_raw_pixels(&png_data);
+        assert!(result.is_ok());
+        
+        let pixels = result.unwrap();
+
+        assert!(pixels.len > 0);
+        assert_eq!(pixels.width, 1);
+        assert_eq!(pixels.height, 1);
+        assert!(pixels.channels == 3 || pixels.channels == 4);
+
+        free_raw_pixels(pixels);
+    }
+    
+    #[test]
+    fn test_dds_conversion() {
+        let test_data = vec![0; 100];
+        let converter = ImageConverter::new(Format::Dds);
+        let result = converter.convert(&test_data);
+        assert!(result.is_err());
+    }
+    
+    #[test]
+    fn test_farbfeld_conversion() {
+        let test_data = vec![0; 100];
+        let converter = ImageConverter::new(Format::Farbfeld);
+        let result = converter.convert(&test_data);
+        assert!(result.is_err());
     }
 }
